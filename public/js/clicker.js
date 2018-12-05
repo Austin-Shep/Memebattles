c2182516ea149addc98c787270ef088fb111a807
Justin Singh <justin.singh125125@gmail.com>
2018-12-04 18:46:25 -0800

    added validation and support for points page

--- public/js/clicker.js
+++ public/js/clicker.js
@@ -1,110 +1,137 @@
 $(document).ready(function () {
-  //on page load, get the id so we can assign it to all the nav bars
-  var id;
-  var currentPoints;
-  var currentClickPower;
-  getId();
+  //loads data for the page to render all data
+  renderPageData();
 
-  function getId() {
+
+  //everytime the user click the get points button, this executes
+  $("#get-points").on("click", function () {
+    //calls the function to increase points for the current user
+    increaseUserPoints();
+  })
+
+  $(".click-upgrade").on("click", function () {
+    //stores the button clicked in an object and passses it to function so it can be referenced
+    var currentClickedButton = $(this);
+    //executes the buying process with validation
+    buyClickerUpgrade(currentClickedButton);
+  })
+
+  function renderPageData() {
+    //ajax call to grab current user data
     $.ajax("/api/user/id", {
       type: "GET"
     }).then(function (user) {
-      currentPoints = user[0].points;
+      //stores current user points in variable
+      var currentPoints = user[0].points;
+      //stores current user click power in variable
       currentClickPower = user[0].clickPower;
-      id = user[0].id;
-
-
-      $("#meme-points").text(user[0].points);
-      $("#clickPower").text("Click Power: " + user[0].clickPower);
-
-      checkButtons(user[0].clickPower);
+      //sets the text for how much money a user has
+      $("#meme-points").text(currentPoints);
+      //sets text for the users click power on the html page
+      $("#clickPower").text("Click Power: " + currentClickPower);
       //validation for the click buttons
+      checkButtons(currentClickPower);
     });
   }
-  //everytime the user click the get points button, this executes
-  $("#get-points").on("click", function () {
-    var morePoints;
 
-    $.ajax("/api/get-current-power", {
-      type: "GET"
-    }).then(function (data) {
-      console.log("/n/n/n/n");
-      console.log("current power test");
-      console.log(data);
-      if (data[0].clickPower == 1) {
-        morePoints = 0.01;
-      }
-
-      if (data[0].clickPower == 2) {
-        morePoints = 0.05;
-      }
-
-      if (data[0].clickPower == 3) {
-        morePoints = 0.10;
-      }
-
-      if (data[0].clickPower == 4) {
-        morePoints = 1.00;
-      }
-      if (data[0].clickPower == 5) {
-        morePoints = 2.00;
-      }
-
-      morePoints = parseFloat(morePoints);
-      currentPoints = parseFloat(currentPoints);
-      currentPoints += morePoints;
-
-      var points = {
-        points: currentPoints
-      };
-
-      $.ajax("/api/user/id", {
-        type: "put",
-        data: points
-      }).then(function () {
-        getId();
-      });
-    });
-  })
-
-  //code below here is for the buttons for increasing click
-
-  $(".click-upgrade").on("click", function () {
-
-    //get current user points
-    $.ajax("/api/get-current-user-points", {
-      type: "GET"
-    }).then(function (data) {
-      var currentUserPoints = parseFloat(data[0].points);
-      var subtractPoints = parseFloat($(this).attr("data-cost"));
-      console.log("test");
-
-
-
-
-
-
-      var upgradeClickPower = {
-        clickPower: $(this).attr("data-clickPower")
-      };
-
-      $.ajax("/upgrade-click", {
-        type: "PUT",
-        data: upgradeClickPower
-      }).then(function (data) {
-        getId();
-      })
-
-    })
-
-  });
 
   function checkButtons(userClickPower) {
+    //this checks all the buttons and makes sure to turn off buttons when user is a certain click level
     for (var i = 2; i <= 5; i++) {
       if (userClickPower >= $(`button[data-clickPower=${i}]`).data().clickpower) {
         $(`button[data-clickPower=${i}]`).attr('disabled', "disabled");
         $(`button[data-clickPower=${i}]`).css("background", "gray");
       }
     }
+  }
+
+  function buyClickerUpgrade(currentClickedButton) {
+
+
+    //get current user points
+    $.ajax("/api/get-current-user-points", {
+      type: "GET"
+    }).then(function (data) {
+
+      //I parse variables as floats to prevent string concatination of numbers
+      var currentUserPoints = parseFloat(data[0].points);
+      var subtractPoints = parseFloat(currentClickedButton.attr("data-cost"));
+
+      //if user has less money than they can afford
+      if ((currentUserPoints - subtractPoints) < 0) {
+        alert("Insufficent Meme Tokens");
+
+      }
+      //if user has enough money 
+      else {
+        //subtract userpoints from there purchase
+        var temp = currentUserPoints - subtractPoints;
+
+        //send over the new click power
+        var upgradeClickPower = {
+          clickPower: currentClickedButton.attr("data-clickPower"),
+          points: temp
+        };
+        $.ajax("/upgrade-click", {
+          type: "PUT",
+          data: upgradeClickPower
+        }).then(function (data) {
+          renderPageData();
+        })
+      }
+
+    })
+  }
+
+
+  function increaseUserPoints() {
+    //holds amount of points to be added on based on users click power
+    var morePoints = 0.0;
+
+    $.ajax("/api/get-current-power", {
+      type: "GET"
+    }).then(function (data) {
+      var currentClickPower = data[0].clickPower;
+      var currentPoints = parseFloat(data[0].points);
+
+      //series of if statments checking current users click power
+      //so we can add correct amount of points per click
+      if (currentClickPower == 1) {
+        morePoints = 0.01;
+      }
+
+      if (currentClickPower == 2) {
+        morePoints = 0.05;
+      }
+
+      if (currentClickPower == 3) {
+        morePoints = 0.10;
+      }
+
+      if (currentClickPower == 4) {
+        morePoints = 1.00;
+      }
+      if (currentClickPower == 5) {
+        morePoints = 2.00;
+      }
+
+      //adds points per click to the users current points 
+      currentPoints += morePoints;
+
+      //store points as an object because that is how the req.body accepts variables
+      var points = {
+        points: currentPoints
+      };
+
+      //ajax call that updates the databases current points
+      $.ajax("/api/user/id", {
+        type: "put",
+        data: points
+      }).then(function () {
+        //we call this function so we dont have to re render the whole page again to see changed data
+        //this function manually sets text
+        renderPageData();
+      });
+    });
   }
 });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,10 +4,10 @@ var passport = require("../config/passport");
 
 var currentId;
 
-module.exports = function(app) {
+module.exports = function (app) {
   //this portion of code is all for passport to work
 
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
@@ -24,41 +24,41 @@ module.exports = function(app) {
   //^^^^^^^^^^^^^^
 
   //this route is for storing into the database new memes
-  app.post("/api/manager", function(req, res) {
+  app.post("/api/manager", function (req, res) {
     //send this data to the meme table
-    db.Memes.create(req.body).then(function(data) {
+    db.Memes.create(req.body).then(function (data) {
       res.json(data);
     });
   });
 
   //a get route for the memes
-  app.get("/api/manager", function(req, res) {
-    db.Memes.findAll({}).then(function(data) {
+  app.get("/api/manager", function (req, res) {
+    db.Memes.findAll({}).then(function (data) {
       res.json(data);
     });
   });
 
   //sends back the currently signed in user
-  app.get("/api/user/id", function(req, res) {
+  app.get("/api/user/id", function (req, res) {
     console.log("test " + currentId);
     db.User.findAll({
       where: {
         id: req.user.id
       }
-    }).then(function(data) {
+    }).then(function (data) {
       console.log(data);
       res.json(data);
     });
   });
 
   //allows us to upadte which meme belongs to a signed in account
-  app.post("/api/user/id", function(req, res) {
-    db.Boughten_Memes.create(req.body).then(function(data) {
+  app.post("/api/user/id", function (req, res) {
+    db.Boughten_Memes.create(req.body).then(function (data) {
       res.json(data);
     });
   });
   //allows us to update the currently signed in users points
-  app.put("/api/user/id", function(req, res) {
+  app.put("/api/user/id", function (req, res) {
     console.log(req.body);
     db.User.update(
       {
@@ -69,51 +69,52 @@ module.exports = function(app) {
           id: req.user.id
         }
       }
-    ).then(function(data) {
+    ).then(function (data) {
       res.json(data);
     });
   });
 
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
 
   //this route handles upgrading the click power
-  app.put("/upgrade-click", function(req, res) {
+  app.put("/upgrade-click", function (req, res) {
     console.log("\n\n\n\n\n\n\n");
     console.log("clickPower");
     console.log(req.body.clickPower);
 
     db.User.update(
       {
-        clickPower: req.body.clickPower
+        clickPower: req.body.clickPower,
+        points: req.body.points
       },
       {
         where: {
           id: req.user.id
         }
       }
-    ).then(function(data) {
+    ).then(function (data) {
       res.json(data);
     });
   });
 
-  app.get("/api/get-current-power", function(req, res) {
+  app.get("/api/get-current-power", function (req, res) {
     db.User.findAll({
       where: {
         id: req.user.id
       }
-    }).then(function(data) {
+    }).then(function (data) {
       res.json(data);
     });
   });
-  app.get("/api/get-current-user-points", function(req, res) {
+  app.get("/api/get-current-user-points", function (req, res) {
     db.User.findAll({
       where: {
         id: req.user.id
       }
-    }).then(function(data) {
+    }).then(function (data) {
       res.json(data);
     });
   });
