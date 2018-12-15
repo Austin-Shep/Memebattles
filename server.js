cb97717fb6ec2473da64fc44af3f715aa0a8a74d
ewu <es.nj.wu@gmail.com>
2018-12-14 21:23:34 -0800

    fixed layout of battle page

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(passport.session());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
